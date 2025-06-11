890bed78083b3c74e3dd501763b05f40baae105d
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-11 20:37:20 -0300

    agregue el maquetado del componete formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,9 +1,17 @@
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+
 const FormularioTarea = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Form>
+        <Form.Group className="d-flex mb-3">
+          <Form.Control type="text" placeholder="name@example.com" />
+          <Button variant="info">Enviar</Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 };
 
 export default FormularioTarea;
