4abcc3be1482b24d9dff2c146cd336fc74660b87
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-11 21:33:17 -0300

    guardamos el value del input en el useState

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,14 +1,29 @@
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import ListaTarea from "./ListaTarea";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("aqui deberia guardar la tarea");
+  };
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="d-flex mb-3">
-          <Form.Control type="text" placeholder="ingresa una tarea" />
-          <Button variant="info">Enviar</Button>
+          <Form.Control
+            type="text"
+            placeholder="ingresa una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="info" type="submit">
+            Enviar
+          </Button>
         </Form.Group>
       </Form>
       <ListaTarea />
