343b7ea2d7b8efefdc62a8234126496dd0477725
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-23 21:19:38 -0300

    agregamos validaciones al inputTarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -2,22 +2,29 @@ import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import ListaTarea from "./ListaTarea";
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 const FormularioTarea = () => {
-  const [tarea, setTarea] = useState("");
   const tareasLocalstorage =
     JSON.parse(localStorage.getItem("listaTareas")) || [];
   const [tareas, setTareas] = useState(tareasLocalstorage);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
   useEffect(() => {
     console.log("desde use efect");
     localStorage.setItem("listaTareas", JSON.stringify(tareas));
   }, [tareas]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const agregarTareas = (data) => {
     console.log("aqui deberia guardar la tarea");
-    setTareas([...tareas, tarea]);
-    setTarea("");
+    console.log(data.inputTarea);
+    setTareas([...tareas, data.inputTarea]);
+    reset();
   };
 
   const borrarTarea = (nombreTarea) => {
@@ -27,18 +34,36 @@ const FormularioTarea = () => {
 
   return (
     <section>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="d-flex mb-3">
+      <Form onSubmit={handleSubmit(agregarTareas)}>
+        <Form.Group className="d-flex mb-1">
           <Form.Control
             type="text"
             placeholder="ingresa una tarea"
             onChange={(e) => setTarea(e.target.value)}
-            value={tarea}
+            {...register("inputTarea", {
+              required: "La tarea es un dato obligatorio",
+              minLength: {
+                value: 3,
+                message: "La tarea debe tener 3 caracteres como minimo",
+              },
+              maxLength: {
+                value: 50,
+                message: "La tarea debe tener 50 caracteres como maximo",
+              },
+              pattern: {
+                value: /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{1,50}$/,
+                message:
+                  "La tarea debe contener caracteres alfa numericos mayusculas o minusculas",
+              },
+            })}
           />
           <Button variant="info" type="submit">
             Enviar
           </Button>
         </Form.Group>
+        <Form.Text className="mb-3 text-danger">
+          {errors.inputTarea?.message}
+        </Form.Text>
       </Form>
       <ListaTarea tareas={tareas} borrarTarea={borrarTarea} />
     </section>
